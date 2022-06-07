88af785df39607827913c1eb84e303f0ba1aa8c7
Denys Chernyshov <denys.chernyshov@optimax-energy.de>
2022-06-07 16:08:14 +0200

    Init

---
+++ src/test/java/optimax/MainTest.java
@@ -0,0 +1,18 @@
+package optimax;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author Denys Chernyshov
+ * @since 1.0
+ */
+class MainTest {
+
+    @Test
+    void test(){
+        assertTrue(true);
+    }
+
+}
