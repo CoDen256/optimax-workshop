705deb5c752ee154b45c817ff5d82a77c19d33d1
Denys Chernyshov <denys.chernyshov@optimax-energy.de>
2022-06-09 16:22:51 +0200

    add solution and isSolved test case

--- src/main/java/optimax/WordleGame.java
+++ src/main/java/optimax/WordleGame.java
@@ -12,24 +12,32 @@ import java.util.Collections;
  */
 public final class WordleGame {
 
+    private final Word solution;
     private int counter = 0;
     private final Collection<Word> submitted = new ArrayList<>();
     private final Collection<? extends Word> dictionary;
 
-    public WordleGame(Collection<? extends Word> dictionary) {
-        this.dictionary = dictionary;
+    public WordleGame(Word solution, Collection<? extends Word> dictionary) {
+        this.dictionary = requireNonNull(dictionary);
+        this.solution = requireNonNull(solution);
+        if (!dictionary.contains(solution)){
+            throw new IllegalArgumentException(String.format("Solution (%s) is not in dictionary", solution));
+        }
     }
 
     public boolean isFinished() {
-        return counter == 5;
+        return counter == 5 || submitted.contains(solution);
     }
 
     public boolean isSolved() {
-        return false;
+        return submitted.contains(solution);
     }
 
     public void submit(Word guess) {
         requireNonNull(guess);
+        if (submitted.contains(solution)){
+            throw new IllegalStateException(String.format("Game is solved, no more guesses allowed. The solution is: %s", solution));
+        }
         if (counter == 5) {
             throw new IllegalStateException(String.format("Game is finished, no more guesses allowed. Guess submitted: %s", guess));
         }
--- src/test/java/optimax/WordleGameTest.java
+++ src/test/java/optimax/WordleGameTest.java
@@ -6,7 +6,10 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.Set;
 import java.util.stream.Collectors;
 import org.junit.jupiter.api.Test;
 
@@ -18,25 +21,25 @@ class WordleGameTest {
 
     @Test
     void createNewGame_GameIsNotFinished() {
-        WordleGame game = newGame();
+        WordleGame game = new WordleGame(word("valid"), Set.of(word("valid")));
         assertFalse(game.isFinished());
     }
 
     @Test
     void createNewGame_GameIsNotSolved() {
-        WordleGame game = newGame();
+        WordleGame game = new WordleGame(word("valid"), Set.of(word("valid")));
         assertFalse(game.isSolved());
     }
 
     @Test
     void submitNullFails() {
-        WordleGame game = newGame();
+        WordleGame game = new WordleGame(word("valid"), Set.of(word("valid")));
         assertThrows(NullPointerException.class, () -> game.submit(null));
     }
 
     @Test
     void submitWord5Times_GameIsFinished() {
-        WordleGame game = newGame("xxxxx", "yyyyy", "zzzzz");
+        WordleGame game = newFakeGame("xxxxx", "yyyyy", "zzzzz");
         submit(game, "xxxxx", "yyyyy", "zzzzz", "xxxxx");
         assertFalse(game.isFinished());
         submit(game, "yyyyy");
@@ -45,7 +48,7 @@ class WordleGameTest {
 
     @Test
     void submitWordMoreThan5TimesFails() {
-        WordleGame game = newGame("xxxxx");
+        WordleGame game = newFakeGame("xxxxx");
         String guess = "xxxxx";
         // exercise
         submit(game, guess, guess, guess, guess, guess);
@@ -56,20 +59,20 @@ class WordleGameTest {
 
     @Test
     void createNewGame_GetEmptySubmittedGuesses() {
-        WordleGame game = newGame();
+        WordleGame game = newFakeGame();
         assertThat(game.getSubmitted()).isEmpty();
     }
 
     @Test
     void submitGuess_GetSubmittedGuess() {
-        WordleGame game = newGame("xxxxx");
+        WordleGame game = newFakeGame("xxxxx");
         submit(game, "xxxxx");
         assertThat(game.getSubmitted()).containsExactly(word("xxxxx"));
     }
 
     @Test
     void submit2Guesses_Get2SubmittedGuesses() {
-        WordleGame game = newGame("xxxxx", "yyyyy");
+        WordleGame game = newFakeGame("xxxxx", "yyyyy");
         // exercise
         submit(game, "xxxxx", "yyyyy");
         // verify
@@ -78,7 +81,7 @@ class WordleGameTest {
 
     @Test
     void submit3Guesses_Get3SubmittedGuesses() {
-        WordleGame game = newGame("xxxxx", "yyyyy", "zzzzz");
+        WordleGame game = newFakeGame("xxxxx", "yyyyy", "zzzzz");
         // exercise
         submit(game, "xxxxx", "yyyyy", "zzzzz");
         // verify
@@ -91,7 +94,7 @@ class WordleGameTest {
 
     @Test
     void submit2SameGuesses_Get2SameSubmittedGuesses() {
-        WordleGame game = newGame("xxxxx");
+        WordleGame game = newFakeGame("xxxxx");
         // exercise
         submit(game, "xxxxx", "xxxxx");
         // verify
@@ -100,7 +103,7 @@ class WordleGameTest {
 
     @Test
     void submit5Guesses_Get5SubmittedGuesses() {
-        WordleGame game = newGame("xxxxx", "yyyyy", "zzzzz");
+        WordleGame game = newFakeGame("xxxxx", "yyyyy", "zzzzz");
 
         // exercise
         submit(game, "xxxxx", "yyyyy", "zzzzz", "xxxxx", "yyyyy");
@@ -117,7 +120,7 @@ class WordleGameTest {
 
     @Test
     void submitMoreThan5Guesses_Get5SubmittedGuessesAndFail() {
-        WordleGame game = newGame("xxxxx", "yyyyy", "zzzzz", "aaaaa");
+        WordleGame game = newFakeGame("xxxxx", "yyyyy", "zzzzz", "aaaaa");
         String guess = "aaaaa";
 
         // exercise
@@ -136,8 +139,13 @@ class WordleGameTest {
     }
 
     @Test
+    void createNewGameWithNullDictionaryFails() {
+        assertThrows(NullPointerException.class, () -> new WordleGame(word("valid"), null));
+    }
+
+    @Test
     void createNewGameWithDictionary_AcceptWord() {
-        WordleGame game = newGame("xxxxx");
+        WordleGame game = newFakeGame("xxxxx");
         // exercise
         assertDoesNotThrow(() -> submit(game, "xxxxx"));
         // verify
@@ -145,8 +153,19 @@ class WordleGameTest {
     }
 
     @Test
+    void createNewGameWithDictionary_RejectsWordsThatAreNotInDictionary() {
+        WordleGame game = newFakeGame("xxxxx");
+        // exercise
+        assertThrows(IllegalArgumentException.class, () -> submit(game, "yyyyy"));
+        assertThrows(IllegalArgumentException.class, () -> submit(game, "zzzzz"));
+        assertThrows(IllegalArgumentException.class, () -> submit(game, "aaaaa"));
+        // verify
+        assertThat(game.getSubmitted()).isEmpty();
+    }
+
+    @Test
     void createNewGameWithDictionary_AcceptsOnlyWordsFromDictionary() {
-        WordleGame game = newGame("aaaaa", "bbbbb", "ccccc", "ddddd");
+        WordleGame game = newFakeGame("aaaaa", "bbbbb", "ccccc", "ddddd");
 
         // exercise and verify
         assertDoesNotThrow(() -> submit(game, "aaaaa", "bbbbb"));
@@ -165,8 +184,109 @@ class WordleGameTest {
         ).inOrder();
     }
 
-    private WordleGame newGame(String... dict) {
-        return new WordleGame(Arrays.stream(dict).map(Word::new).collect(Collectors.toSet()));
+    @Test
+    void createNewGameWithDictionaryCaseInsensitive() {
+        WordleGame game = newFakeGame("Aaaaa", "BBBBB", "CcCcC", "dddDD");
+
+        // exercise and verify
+        assertDoesNotThrow(() -> submit(game, "aaaaA", "bbbbb", "cCcCc", "dDDdd"));
+
+        assertThat(game.getSubmitted()).containsExactly(
+                word("aaaaa"),
+                word("bbbbb"),
+                word("ccccc"),
+                word("ddddd")
+        ).inOrder();
+    }
+
+    @Test
+    void createGameWithTarget() {
+        assertDoesNotThrow(() -> newGame("targt", "targt"));
+        assertDoesNotThrow(() -> newGame("targt", "abcde", "TARGT"));
+        assertDoesNotThrow(() -> newGame("TARGT", "aaaaa", "bbbbb", "targT"));
+    }
+
+    @Test
+    void createGameWithTargetThatIsNotInDictFails() {
+        assertThrows(IllegalArgumentException.class, () -> newGame("targt"));
+        assertThrows(IllegalArgumentException.class, () -> newGame("targt", "trget", "xxxxx"));
+        assertThrows(IllegalArgumentException.class, () -> newGame("targt", "aaaaa", "abcde"));
+    }
+
+    @Test
+    void createGameWithNullTargetFails() {
+        assertThrows(NullPointerException.class, () -> new WordleGame(null, Set.of(word("valid"))));
+    }
+
+    @Test
+    void submitSomethingAfterTargetFails() {
+        WordleGame game = newGame("valid", "valid", "xxxxx", "aaaaa");
+        // exercise
+        submit(game, "xxxxx", "aaaaa");
+        submit(game, "valid");
+        // verify
+        assertThrows(IllegalStateException.class, () -> submit(game, "xxxxx"));
+        assertThrows(IllegalStateException.class, () -> submit(game, "valid"));
+        assertThrows(IllegalStateException.class, () -> submit(game, "aaaaa"));
+    }
+
+    @Test
+    void gameIsFinishedAndSolvedAfterTargetSubmitted() {
+        WordleGame game = newGame("valid", "valid", "xxxxx", "aaaaa", "zzzzz");
+
+        submit(game, "xxxxx", "aaaaa", "xxxxx");
+        assertFalse(game.isSolved());
+        assertFalse(game.isFinished());
+
+        submit(game, "valid");
+        assertTrue(game.isSolved());
+        assertTrue(game.isFinished());
+
+        assertThrows(IllegalStateException.class, () -> submit(game, "valid"));
+
+        assertThat(game.getSubmitted()).containsExactly(
+                word("xxxxx"),
+                word("aaaaa"),
+                word("xxxxx"),
+                word("valid")
+        ).inOrder();
+    }
+
+    @Test
+    void gameIsNotSolvedIfNoTargetInserted() {
+        WordleGame game = newGame("valid", "valid", "xxxxx", "aaaaa", "zzzzz");
+
+        submit(game, "xxxxx", "aaaaa", "xxxxx");
+        assertFalse(game.isSolved());
+        assertFalse(game.isFinished());
+        submit(game, "xxxxx", "zzzzz");
+        assertFalse(game.isSolved());
+        assertTrue(game.isFinished());
+
+        assertThrows(IllegalStateException.class, () -> submit(game, "valid"));
+
+        assertThat(game.getSubmitted()).containsExactly(
+                word("xxxxx"),
+                word("aaaaa"),
+                word("xxxxx"),
+                word("xxxxx"),
+                word("zzzzz")
+        ).inOrder();
+    }
+
+    private WordleGame newFakeGame(String... dict) {
+        Collection<String> resultDict = new ArrayList<>(Arrays.asList(dict));
+        String fakeTarget = "valid";
+        resultDict.add(fakeTarget);
+        return newGame(fakeTarget, resultDict);
+    }
+
+    private WordleGame newGame(String solution, String... dict) {
+        return new WordleGame(word(solution), Arrays.stream(dict).map(Word::new).collect(Collectors.toSet()));
+    }
+
+    private WordleGame newGame(String solution, Collection<String> dict) {
+        return new WordleGame(word(solution), dict.stream().map(Word::new).collect(Collectors.toSet()));
     }
 
     private Word word(String word) {
