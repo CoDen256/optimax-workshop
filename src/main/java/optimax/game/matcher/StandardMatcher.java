6a96bc437767740067a7889578ce05a64fea885c
Denys Chernyshov <denys.chernyshov@optimax-energy.de>
2022-06-13 15:47:15 +0200

    Refactor

--- src/main/java/optimax/game/matcher/StandardMatcher.java
+++ src/main/java/optimax/game/matcher/StandardMatcher.java
@@ -1,12 +1,15 @@
 package optimax.game.matcher;
 
+import static java.util.function.Predicate.not;
 import static optimax.game.matcher.Match.ABSENT;
 import static optimax.game.matcher.Match.CORRECT;
 import static optimax.game.matcher.Match.WRONG;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import optimax.game.Word;
 
 /**
@@ -21,38 +24,73 @@ import optimax.game.Word;
  * </ul>
  * <b>Note:</b> Multiple occurrences of the same letter(which exists only once in the expected word) do not produce
  * the same match. For example for expected word 'valid' and actual 'axaid' produces 'WRONG, ABSENT, <b>ABSENT</b>, CORRECT, CORRECT'
+ *
  * @author Denys Chernyshov
  * @since 1.0
  */
 public class StandardMatcher implements WordMatcher {
     @Override
-    public MatchResult match(Word expected, Word actual) {
-        List<String> solutionChars = new ArrayList<>(Arrays.asList(expected.word().split("")));
-        List<String> guessChars =  new ArrayList<>(Arrays.asList(actual.word().split("")));
+    public MatchResult match(Word expectedWord, Word actualWord) {
+        List<WordCharacter> expected = indexed(expectedWord.word());
+        List<WordCharacter> actual = indexed(actualWord.word());
+        List<WordCharacter> alreadyMatched = new ArrayList<>();
 
-        String matched = "matched";
-        String wronged = "wronged";
-        Match[] matches = new Match[]{CORRECT, CORRECT, CORRECT, CORRECT, CORRECT};
-        for (int i = 0; i < 5; i++) {
-            if (solutionChars.get(i).equals(guessChars.get(i))){
-                solutionChars.set(i, matched);
+        Match[] matches = new Match[]{ABSENT, ABSENT, ABSENT, ABSENT, ABSENT};
+
+        for (WordCharacter actualChar : actual) {
+            if (expected.contains(actualChar)) {
+                alreadyMatched.add(actualChar);
+                matches[actualChar.pos] = CORRECT;
             }
         }
 
-        for (int i = 0; i < 5; i++) {
-            String target = solutionChars.get(i);
-            String guess = guessChars.get(i);
-            if (target.equals(matched)) continue;
-
-            int indx = solutionChars.indexOf(guess);
-            if (!target.equals(guess) && indx != -1) {
-                solutionChars.set(indx, wronged);
-                matches[i] = WRONG;
-            }
-            else if (!target.equals(guess)){
-                matches[i] = ABSENT;
-            }
+        for (WordCharacter actualChar : actual) {
+            if (alreadyMatched.contains(actualChar)) continue;
+            expected.stream()
+                    .filter(not(alreadyMatched::contains))
+                    .filter(expectedChar -> isAtWrongPosition(actualChar, expectedChar))
+                    .findFirst()
+                    .ifPresent(match -> {
+                        alreadyMatched.add(match);
+                        matches[actualChar.pos] = WRONG;
+                    });
         }
+
         return new MatchResult(matches);
+    }
+
+    private boolean isAtWrongPosition(WordCharacter a, WordCharacter b) {
+        return b.character == a.character && b.pos != a.pos;
+    }
+
+    private List<WordCharacter> indexed(String word) {
+        return IntStream.range(0, word.length())
+                .boxed()
+                .map(i -> new WordCharacter(word.charAt(i), i))
+                .collect(Collectors.toList());
+    }
+
+    private static class WordCharacter {
+
+        private final char character;
+        private final int pos;
+
+        public WordCharacter(char character, int pos) {
+            this.character = character;
+            this.pos = pos;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            WordCharacter that = (WordCharacter) o;
+            return character == that.character && pos == that.pos;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(character, pos);
+        }
     }
 }
